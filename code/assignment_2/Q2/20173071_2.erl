67a848e96741fd4885d8272f2c42d2b59ee1f081
Aditya Saripalli <aditya.saripalli@students.iiit.ac.in>
2021-02-21 23:19:21 +0530

    A-2: Q2 Some fixes.

--- code/assignment_2/Q2/20173071_2.erl
+++ code/assignment_2/Q2/20173071_2.erl
@@ -10,14 +10,9 @@ start([InputFile, OutputFile]) ->
     {P, V, E, G, S} = parse_input(InputFile),
     X = initialize_distances(V, []),
     log_data(P, V, E, G, S, X),
-    {Is_negative_cycle_present, D} = bellman_ford(P, V, E, G, S, X),
-    if
-        Is_negative_cycle_present == true ->
-            io:format("[ERROR] The input graph contains a -ve cycle~n");
-        Is_negative_cycle_present == false ->
-            write_output_data(D, OutputFile),
-            io:format("[INFO] Output written to ~p~n", [OutputFile])
-    end.
+    D = bellman_ford(P, V, E, G, S, X),
+    write_output_data(D, OutputFile),
+    io:format("[INFO] Output written to ~p~n", [OutputFile]).
 
 parse_input(InputFile) ->
     io:format("[INFO] Parsing input file: ~p~n", [InputFile]),
@@ -48,8 +43,8 @@ create_graph(IFH, 0, G) ->
 create_graph(IFH, E, G) ->
     {ok, Line} = file:read_line(IFH),
     {Us,Vs,Ws} = list_to_tuple(string:tokens(Line, "\n| ")),
-    create_graph(IFH, E-1, G ++ [{list_to_integer(Us), list_to_integer(Vs), list_to_integer(Ws)}]).
-
+    {U,V,W} = {list_to_integer(Us), list_to_integer(Vs), list_to_integer(Ws)},
+    create_graph(IFH, E-1, G ++ [{U,V,W}] ++ [{V,U,W}]).
 
 % Initialize the Distances from Source to each vertex
 initialize_distances(0, D) -> list_to_tuple(D);
@@ -60,8 +55,7 @@ bellman_ford(P, V, E, G, S, D) ->
     io:format("[INFO] Running Bellman-Ford on input graph~n"),
     D1 = setelement(S,D,0),
     D2 = relax(1, V, E, G, D1),
-    Status = check_negative_cycle(1, E, G, D2),
-    {Status, D2}.
+    D2.
 
 % Relax all the edges |V|-1 times
 relax(X, V, E, G, D) ->
@@ -74,10 +68,10 @@ relax(X, V, E, G, D) ->
 
 relax_edges(Y, E, G, D) ->
     if
-        Y =< E  ->
+        Y =< 2*E  ->
             D1 = update_distances(Y, G, D),
             relax_edges(Y+1, E, G, D1);
-        Y > E -> D
+        Y > 2*E -> D
     end.
 
 update_distances(Y, G, D) ->
@@ -88,20 +82,6 @@ update_distances(Y, G, D) ->
         (Dist_U =/= inf) and (Dist_U + W < Dist_V) ->
             setelement(element(2,element(Y,G)), D, Dist_U + W);
         true -> D
-    end.
-
-% Check for Ny negative weight cycles in the graph
-check_negative_cycle(X, E, G, D) ->
-    if
-        X =< E ->
-            Dist_U = element(element(1,element(X,G)),D),
-            Dist_V = element(element(2,element(X,G)),D),
-            W = element(3,element(X,G)),
-            if
-                (Dist_U =/= inf) and (Dist_U + W < Dist_V) -> true;
-                true -> check_negative_cycle(X+1, E, G, D)
-            end;
-        X > E -> false
     end.
 
 % Logging debug data
--- code/assignment_2/erlang_test_programs/graph.erl
+++ code/assignment_2/erlang_test_programs/graph.erl
@@ -1,15 +1,111 @@
--module(graph).
--export([info/0, start/1]).
+% Course : Distributed Systems
+% Assignment : 2, Q2
+% Author: Aditya Saripalli
+% Description: Implementing Bellman-Ford Algorithm with concurrency in Erlang
 
-info() -> io:format("Computing shortest paths in a weighted graph in Erlang.~n").
+-module('graph').
+-export([dijkstra/1]).
 
-% foreach loop construct to traverse a list
-foreach(List) -> foreach(List, 0).
-foreach([], Index) -> Index;
-foreach([H|T], Index) ->
-    % Do Something with the element stored in H
-    io:format("List[~w]:~w~n", [Index, H]),
-    foreach(T, Index+1).
+dijkstra([InputFile, OutputFile]) ->
+    {P, V, E, G, S} = parse_input(InputFile),
+    X = initialize_distances(V, []),
+    log_data(P, V, E, G, S, X).
+    %D = compute_sssp(P, V, E, G, S, X),
+    %write_output_data(D, OutputFile),
 
+parse_input(InputFile) ->
+    io:format("[INFO] Parsing input file: ~p~n", [InputFile]),
+    {ok, IFH} = file:open(InputFile, [read]),
 
-start(L) -> foreach(L).
+    % read the number of processes
+    {ok, Line1} = file:read_line(IFH),
+    P = list_to_integer(element(1, list_to_tuple(string:tokens(Line1, "\n")))),
+
+    % read the number of Vertices and Edges
+    {ok, Line2} = file:read_line(IFH),
+    {Vs,Es} = list_to_tuple(string:tokens(Line2, "\n| ")),
+    {V,E} = {list_to_integer(Vs), list_to_integer(Es)},
+
+    % Create the graph
+    {S, G} = create_graph(IFH, E),
+    {P, V, E, G, S}.
+
+% Create the graph from the inout data
+create_graph(IFH, E) ->
+    io:format("[INFO] Creating graph from the input data~n"),
+    create_graph(IFH, E, #{}).
+create_graph(IFH, 0, G) ->
+    {ok, Line} = file:read_line(IFH),
+    S = list_to_integer(element(1, list_to_tuple(string:tokens(Line, "\n")))),
+    {S, G};
+create_graph(IFH, E, G) ->
+    {ok, Line} = file:read_line(IFH),
+    {Us,Vs,Ws} = list_to_tuple(string:tokens(Line, "\n| ")),
+    {U,V,W} = {list_to_integer(Us), list_to_integer(Vs), list_to_integer(Ws)},
+    Val1 = maps:get(U,G,[]) ++ [{V,W}],
+    Val2 = maps:get(V,G,[]) ++ [{U,W}],
+    G1 = maps:put(U, Val1, G),
+    G2 = maps:put(V, Val2, G1),
+    create_graph(IFH, E-1, G2).
+
+% Initialize the Distances from Source to each vertex
+initialize_distances(0, D) -> list_to_tuple(D);
+initialize_distances(V, D) -> initialize_distances(V-1, D ++ [{inf}]).
+
+% Initiate Bellman-Ford Algorithm
+%compute_sssp(P, V, E, G, S, D) ->
+%    io:format("[INFO] Running Dijkstra on input graph~n"),
+%    D1 = setelement(S,D,0),
+%    D2 = relax(1, V, E, G, D1),
+%    {Status, D2}.
+%
+%% Relax all the edges |V|-1 times
+%relax(X, V, E, G, D) ->
+%    if
+%        X < V  ->
+%            D1 = relax_edges(1, E, G, D),
+%            relax(X+1, V, E, G, D1);
+%        X == V -> D
+%    end.
+%
+%relax_edges(Y, E, G, D) ->
+%    if
+%        Y =< E  ->
+%            D1 = update_distances(Y, G, D),
+%            relax_edges(Y+1, E, G, D1);
+%        Y > E -> D
+%    end.
+%
+%update_distances(Y, G, D) ->
+%    Dist_U = element(element(1,element(Y,G)),D),
+%    Dist_V = element(element(2,element(Y,G)),D),
+%    W = element(3,element(Y,G)),
+%    if
+%        (Dist_U =/= inf) and (Dist_U + W < Dist_V) ->
+%            setelement(element(2,element(Y,G)), D, Dist_U + W);
+%        true -> D
+%    end.
+
+% Logging debug data
+log_data(P, V, E, G, S, D) ->
+    io:format("[INFO] No of Processes: ~p~n", [P]),
+    io:format("[INFO] No of Vertices: ~p~n", [V]),
+    io:format("[INFO] No of Edges: ~p~n", [E]),
+    io:format("[INFO] Input Graph: ~p~n", [G]),
+    io:format("[INFO] Source Vertex: ~p~n", [S]),
+    io:format("[INFO] Distances initialized to: ~p~n", [D]).
+
+% Write the output to output.txt file
+%write_output_data(D, OutputFile) ->
+%    {ok, OFH} = file:open(OutputFile, [write]),
+%    io:format(OFH, "Vertex    Distance From Source~n", []),
+%    print_distances(OFH, D, 1).
+%
+%print_distances(OFH, D, X) ->
+%    Size = tuple_size(D),
+%    if
+%        X =< Size ->
+%            io:format(OFH, "  ~p                 ~p~n", [X, element(X,D)]),
+%            print_distances(OFH, D, X+1);
+%        X > Size -> ok
+%    end.
